7dd8d1fb4605089630981c7e5da10f695f9a55b7
truong99 <truongnguyen30999@gmail.com>
2020-12-07 21:46:53 +0700

    overnight cung anh em

--- Quan_ly_phong_cho_thue/backend/models/Bill.model.js
+++ Quan_ly_phong_cho_thue/backend/models/Bill.model.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
  
 const billSchema = mongoose.Schema({
     RoomId: { type: mongoose.Schema.Types.ObjectId, required: true },
-    RoomNumber:{type :Number},//
+    RoomNumber:{type :Number},
     ElectricFee: {type: Number},
     WaterFee: {type: Number},
-    RoomPrice: {type: Number},//
+    RoomPrice: {type: Number},
     TotalBill: {type: Number},
-    DateCreate:{type: Date},//
+    DateCreate:{type: Date},
     OtherCosts:{type: String},
     StartDate: {type: Date},
     EndDate: {type: Date},
